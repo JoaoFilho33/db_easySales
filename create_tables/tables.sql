6a9a29dde0a65fe4c2e9aed6dbb76411d3be3bd1
JoaoFilho33 <joaod.almeidafi@gmail.com>
2023-06-11 21:22:50 -0300

    tables atuais

--- create_tables/tables.sql
+++ create_tables/tables.sql
@@ -1,28 +1,29 @@
 CREATE TABLE Cliente (
   	id_cli          SERIAL NOT NULL PRIMARY KEY,
   	nome_cli        VARCHAR(40) NOT NULL,
-  	cpf             VARCHAR(15),
+  	cpf             VARCHAR(15) NOT NULL,
   	bairro_cli      VARCHAR(15) NOT NULL,
   	num_cli         VARCHAR(12) NOT NULL,
   	logradouro_cli  VARCHAR(25) NOT NULL,
-  	cep_cli         VARCHAR(11) NOT NULL,
+  	cep_cli         VARCHAR(9) NOT NULL,
   	cidade_cli      VARCHAR(20) NOT NULL,
-	email_cli		VARCHAR(25),
-	telefone		VARCHAR(12) NOT NULL
+	email_cli		VARCHAR(25) NOT NULL,
+	telefone		VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE Fornecedor (
   	id_fn             SERIAL NOT NULL PRIMARY KEY,
   	nome_fn           VARCHAR(40) NOT NULL,
-  	bairro_fn         VARCHAR(20) NOT NULL,
-  	num_fn            VARCHAR(13) NOT NULL,
-  	logradouro_fn     VARCHAR(15) NOT NULL,
-  	cep_fn            VARCHAR(13) NOT NULL,
+  	bairro_fn         VARCHAR(25) NOT NULL,
+  	num_fn            VARCHAR(20) NOT NULL,
+  	logradouro_fn     VARCHAR(30) NOT NULL,
+  	cep_fn            VARCHAR(9) NOT NULL,
   	cidade_fn         VARCHAR(20) NOT NULL,
 	telefone_fn		  VARCHAR(15) NOT NULL,
-	cpf_cnpj_fn		  VARCHAR(15) NOT NULL,
-	email_fn		  VARCHAR(25)
+	cpf_fn			  VARCHAR(18) NOT NULL,
+	email_fn		  VARCHAR(25) NOT NULL
 );
+SELECT * FROM FORNECEDOR
 
 CREATE TABLE Compra (
   	id_compra               SERIAL NOT NULL PRIMARY KEY,
@@ -31,40 +32,16 @@ CREATE TABLE Compra (
   	valor_total_compra      FLOAT NOT NULL
 );
 
-CREATE TABLE ItemCompra (
-  	compraId                   INT NOT NULL REFERENCES Compra(id_compra),
-  	estoqueId                  INT NOT NULL REFERENCES Estoque(id_estoque),
-  	qtdComprada                INT NOT NULL,
-	valor_total_item 		   FLOAT NOT NULL,
-	preco_prod_c			   REAL NOT NULL,
-  	--dt_compra                TIMESTAMPTZ NOT NULL,
-  	CONSTRAINT PRI_ITEM_COMPRA PRIMARY KEY (compraId, estoqueId)
-);
-
-CREATE TABLE Venda (
-  	id_venda               SERIAL NOT NULL PRIMARY KEY,
-  	cli_venda_id           INT NOT NULL REFERENCES Cliente(id_cli),
-  	valor_total_venda      REAL NOT NULL,
-  	dt_venda		       TIMESTAMPTZ NOT NULL,
-	qtd_itens			   INT NOT NULL,
-  	qtd_parcelas_total	   INT NOT NULL,
-	qtd_parcelas_falta	   INT
-);
-
-CREATE TABLE ItemVenda (
-	vendaId                INT NOT NULL REFERENCES Venda(id_venda),
-  	estoqueId              INT NOT NULL REFERENCES Estoque(id_estoque),
-	qtd_vendida		   	   INT NOT NULL,
-	valor_total_item_v     REAL NOT NULL,
-  	CONSTRAINT PRI_ITEM_VENDA PRIMARY KEY (estoqueId, vendaId)
-);
-
 CREATE TABLE Empresa (
   	id_emp           SERIAL NOT NULL PRIMARY KEY,
-	cpf_cnpj		 varchar(18),
   	nome_emp         VARCHAR(15) NOT NULL,
   	email_emp        VARCHAR(25) NOT NULL, --CRIAR VIEW DE CADA EMPRESA PARA SEU PROPRIO BD
   	senha_emp        VARCHAR(8) NOT NULL
+);
+
+CREATE TABLE Produto (
+  	id_prod             SERIAL PRIMARY KEY NOT NULL,
+  	nome_prod           VARCHAR NOT NULL
 );
 
 CREATE TABLE Estoque (
@@ -75,7 +52,30 @@ CREATE TABLE Estoque (
 	quant_estq		  INT NOT NULL
 );
 
-CREATE TABLE Produto (
-  	id_prod             SERIAL PRIMARY KEY NOT NULL,
-  	nome_prod           VARCHAR(25) NOT NULL
+CREATE TABLE ItemCompra (
+  	compraId                   INT NOT NULL REFERENCES Compra(id_compra),
+  	estoqueId                  INT NOT NULL REFERENCES Estoque(id_estoque),
+  	qtdComprada                INT NOT NULL,
+	valor_total_item_c 		   FLOAT NOT NULL,
+	preco_prod_c			   REAL NOT NULL,
+  	CONSTRAINT PRI_ITEM_COMPRA PRIMARY KEY (compraId, estoqueId)
+);
+
+CREATE TABLE Venda (
+  	id_venda               INT NOT NULL PRIMARY KEY,
+  	id_cli                 INT NOT NULL REFERENCES Cliente(id_cli),
+  	valor_total_venda      REAL NOT NULL,
+  	dt_venda	       	   TIMESTAMPTZ NOT NULL,
+	qtd_itens			   INT NOT NULL,
+  	qtd_parcelas_total	   INT NOT NULL,
+  	qtd_parcelas_falta	   INT
+);
+
+
+CREATE TABLE ItemVenda (
+	vendaId                INT NOT NULL REFERENCES Venda(id_venda),
+  	estoqueId              INT NOT NULL REFERENCES Estoque(id_estoque),
+	qtd_vendida		   	   INT NOT NULL,
+	valor_total_item_v     REAL NOT NULL,
+  	CONSTRAINT PRI_ITEM_VENDA PRIMARY KEY (estoqueId, vendaId)
 );
